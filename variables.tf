5cc82ac0a5c985d1a9b6ce36aef80d4aa08e85c0
Joel Serrano <joel@textplus.com>
2022-12-16 14:41:44 -0800

    expose force_new and force_conflicts vars for kubectl_manifest resource

--- variables.tf
+++ variables.tf
@@ -3,8 +3,20 @@ variable "crds_urls" {
   description = "List of the URLs where custom resource definitions are declared."
 }
 
+variable "force_new" {
+  type        = bool
+  default     = false
+  description = "Optional. Forces delete & create of resources if the yaml_body changes. Default false."
+}
+
 variable "server_side_apply" {
   type        = bool
   default     = false
   description = "Optional. Allow using server-side-apply method. Default false."
+}
+
+variable "force_conflicts" {
+  type        = bool
+  default     = false
+  description = "Optional. Allow using force_conflicts. Default false."
 }
